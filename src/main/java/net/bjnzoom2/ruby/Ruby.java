0f1441a3ac3a4b3082ce8544ddc6bb20aff0b1c9
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-14 17:39:13 +0800

    Initial commit

---
+++ src/main/java/net/bjnzoom2/ruby/Ruby.java
@@ -0,0 +1,16 @@
+package net.bjnzoom2.ruby;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ruby implements ModInitializer {
+	public static final String MOD_ID = "ruby";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bjnzoom2/ruby/RubyClient.java
@@ -0,0 +1,10 @@
+package net.bjnzoom2.ruby;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
