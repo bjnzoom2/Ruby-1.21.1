5b46f10ff5c29627747e7da307e31ad5c0e06591
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-17 20:33:42 +0800

    Initial commit

--- src/main/java/net/bjnzoom2/ruby/Ruby.java
+++ src/main/java/net/bjnzoom2/ruby/Ruby.java
@@ -1,5 +1,6 @@
 package net.bjnzoom2.ruby;
 
+import net.bjnzoom2.ruby.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Ruby implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/bjnzoom2/ruby/item/ModItems.java
@@ -0,0 +1,11 @@
+package net.bjnzoom2.ruby.item;
+
+import net.bjnzoom2.ruby.Ruby;
+
+public class ModItems {
+
+
+    public static void registerModItems() {
+        Ruby.LOGGER.info("Registering Mod Items for " + Ruby.MOD_ID);
+    }
+}
