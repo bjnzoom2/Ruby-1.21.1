0133914b65ac981a482d120aa33187db3c51a858
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-18 18:02:20 +0800

    Initial commit

--- src/main/java/net/bjnzoom2/ruby/item/ModItems.java
+++ src/main/java/net/bjnzoom2/ruby/item/ModItems.java
@@ -1,11 +1,25 @@
 package net.bjnzoom2.ruby.item;
 
 import net.bjnzoom2.ruby.Ruby;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
 
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(Ruby.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
         Ruby.LOGGER.info("Registering Mod Items for " + Ruby.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(RUBY);
+        });
     }
 }
