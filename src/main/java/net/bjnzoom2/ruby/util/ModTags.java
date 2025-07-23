abc8070f637df76ee012fafdaf975c8b305a0fab
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-23 20:34:20 +0800

    Initial commit

---
+++ src/main/java/net/bjnzoom2/ruby/util/ModTags.java
@@ -0,0 +1,24 @@
+package net.bjnzoom2.ruby.util;
+
+import net.bjnzoom2.ruby.Ruby;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(Ruby.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(Ruby.MOD_ID, name));
+        }
+    }
+}
