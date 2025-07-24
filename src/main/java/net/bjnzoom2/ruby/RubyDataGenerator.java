e19358779160a5f908a3c9045517c432620ea935
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-24 16:27:37 +0800

    Initial commit

--- src/main/java/net/bjnzoom2/ruby/RubyDataGenerator.java
+++ src/main/java/net/bjnzoom2/ruby/RubyDataGenerator.java
@@ -1,7 +1,9 @@
 package net.bjnzoom2.ruby;
 
 import net.bjnzoom2.ruby.datagen.ModBlockTagProvider;
+import net.bjnzoom2.ruby.datagen.ModItemTagProvider;
 import net.bjnzoom2.ruby.datagen.ModLootTableProvider;
+import net.bjnzoom2.ruby.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -11,6 +13,8 @@ public class RubyDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
 	}
 }
--- src/main/java/net/bjnzoom2/ruby/datagen/ModModelProvider.java
+++ src/main/java/net/bjnzoom2/ruby/datagen/ModModelProvider.java
@@ -1,4 +1,31 @@
 package net.bjnzoom2.ruby.datagen;
 
-public class ModModelProvider {
+import net.bjnzoom2.ruby.block.ModBlocks;
+import net.bjnzoom2.ruby.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_RUBY_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_DEEPSLATE_ORE);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.RUBY, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_RUBY, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.CHISEL, Models.GENERATED);
+    }
 }
