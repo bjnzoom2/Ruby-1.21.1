da49f62d70a84236c6ca637787f7ee4662c9f648
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-25 09:05:28 +0800

    Initial commit

--- src/main/java/net/bjnzoom2/ruby/item/ModItems.java
+++ src/main/java/net/bjnzoom2/ruby/item/ModItems.java
@@ -3,8 +3,7 @@ package net.bjnzoom2.ruby.item;
 import net.bjnzoom2.ruby.Ruby;
 import net.bjnzoom2.ruby.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,6 +13,22 @@ public class ModItems {
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new Item.Settings()));
 
     public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(65)));
+
+    public static final Item RUBY_SWORD = registerItem("ruby_sword",
+            new SwordItem(ModToolMaterials.RUBY, new Item.Settings()
+                    .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.RUBY, 3, -2.4F))));
+    public static final Item RUBY_PICKAXE = registerItem("ruby_pickaxe",
+            new PickaxeItem(ModToolMaterials.RUBY, new Item.Settings()
+                    .attributeModifiers(PickaxeItem.createAttributeModifiers(ModToolMaterials.RUBY, 1, -2.8F))));
+    public static final Item RUBY_SHOVEL = registerItem("ruby_shovel",
+            new ShovelItem(ModToolMaterials.RUBY, new Item.Settings()
+                    .attributeModifiers(ShovelItem.createAttributeModifiers(ModToolMaterials.RUBY, 1.5F, -3.0F))));
+    public static final Item RUBY_AXE = registerItem("ruby_axe",
+            new AxeItem(ModToolMaterials.RUBY, new Item.Settings()
+                    .attributeModifiers(AxeItem.createAttributeModifiers(ModToolMaterials.RUBY, 6, -3.2F))));
+    public static final Item RUBY_HOE = registerItem("ruby_hoe",
+            new HoeItem(ModToolMaterials.RUBY, new Item.Settings()
+                    .attributeModifiers(HoeItem.createAttributeModifiers(ModToolMaterials.RUBY, 0, -3.0F))));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(Ruby.MOD_ID, name), item);
---
+++ src/main/java/net/bjnzoom2/ruby/item/ModToolMaterials.java
@@ -0,0 +1,71 @@
+package net.bjnzoom2.ruby.item;
+
+import com.google.common.base.Suppliers;
+import net.bjnzoom2.ruby.util.ModTags;
+import net.minecraft.block.Block;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    RUBY(ModTags.Blocks.INCORRECT_FOR_RUBY_TOOL,
+            1300, 9.5F, 3.0F, 13, () -> Ingredient.ofItems(ModItems.RUBY));
+
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private ModToolMaterials(
+            final TagKey<Block> inverseTag,
+            final int itemDurability,
+            final float miningSpeed,
+            final float attackDamage,
+            final int enchantability,
+            final Supplier<Ingredient> repairIngredient
+    ) {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = Suppliers.memoize(repairIngredient::get);
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return this.inverseTag;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+}
