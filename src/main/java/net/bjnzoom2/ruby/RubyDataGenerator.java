92bb5e15117160d1dba8bd374e9edfb6bec6fece
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-27 14:47:45 +0800

    Initial commit

--- src/main/java/net/bjnzoom2/ruby/RubyDataGenerator.java
+++ src/main/java/net/bjnzoom2/ruby/RubyDataGenerator.java
@@ -1,8 +1,12 @@
 package net.bjnzoom2.ruby;
 
 import net.bjnzoom2.ruby.datagen.*;
+import net.bjnzoom2.ruby.world.ModConfiguredFeatures;
+import net.bjnzoom2.ruby.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class RubyDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,5 +18,11 @@ public class RubyDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 	}
 }
